2623199c4ad756d7bcaf148ea8d932b26215bde2
Fabio Seixas <flscosta94@gmail.com>
2021-08-20 17:05:24 -0300

    feat(create_thumbnail): update module

--- create_thumbnail.py
+++ create_thumbnail.py
@@ -1,12 +1,23 @@
 
-import numpy as np
-import cv2
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+
+from numpy import asarray
+from cv2 import imdecode, imencode, IMREAD_COLOR
 import boto3
 import os
+
+from resize import resize_dimensions
+
 
 s3 = boto3.resource('s3')
 session = boto3.Session()
 client = session.client('s3')
+
+MEDIUM_MIN_DIMENSION = 300
+SMALL_MIN_DIMENSION = 150
 
 BUCKET_TARGET = os.environ.get('BUCKET_TARGET')
 BUCKET_THUMBNAIL = os.environ.get('BUCKET_THUMBNAIL')
@@ -17,43 +28,49 @@ def get_image(key_image):
     return response['Body']
 
 def buffer_to_array(buffer):
-    image = np.asarray(bytearray(buffer.read()), dtype="uint8")
-    image_array = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = asarray(bytearray(buffer.read()), dtype="uint8")
+    image_array = imdecode(image, IMREAD_COLOR)
     return image_array
 
 def get_image_attribute(key_image):
-    prefix, file = key_image.split('/')
-    image_helper = file.split('.')
-    return image_helper[0], image_helper[1], prefix
+    # file = key_image.split('/')
+    image_helper = key_image.split('.')
+    return image_helper[0], image_helper[1]
 
 
 def generate_buffer(image_array, image_extension):
-    _, image_buffer_array = cv2.imencode('.%s' % image_extension, image_array)
+    _, image_buffer_array = imencode('.%s' % image_extension, image_array)
     byte_image = image_buffer_array.tobytes()
     return byte_image
 
-def create_thumbnail(key_image, size):
+def create_thumbnail(key_image, min_size):
     buffer = get_image(key_image)
     image_array = buffer_to_array(buffer)
-    image_resized = cv2.resize(image_array, size, interpolation=cv2.INTER_AREA)
-    image_name, image_extension, prefix = get_image_attribute(key_image)
+    image_resized, final_width, final_height = resize_dimensions(image_array, min_size)
+    image_name, image_extension = get_image_attribute(key_image)
     image_bytes = generate_buffer(image_resized, image_extension)
 
-    thumbnail_name = '%s_%sx%s.%s' % (image_name, size[0], size[1], image_extension)
+    thumbnail_name = '%s_%s_.%s' % (image_name, min_size, image_extension)
 
-
-    s3.Bucket(BUCKET_THUMBNAIL).put_object(Key='%s/%s' % (prefix, thumbnail_name),
+    s3.Bucket(BUCKET_THUMBNAIL).put_object(Key='%s' % (thumbnail_name),
                                     Body=image_bytes,
                                     ACL='public-read',
                                     ContentType='image/%s' % image_extension)
     
 
 def handler(event, context):
+
+    print('[Enter the Handler]: the event, ', event)
     
     key_image = event['Records'][0]['s3']['object']['key']
-    size = int(114), int(84)
 
-    create_thumbnail(key_image, size)
+    # create medium thumbnail
+    create_thumbnail(key_image, MEDIUM_MIN_DIMENSION)
+    # create small thumbnail
+    create_thumbnail(key_image, SMALL_MIN_DIMENSION)
+
+    print('[Finish Handler] \n')
+    print('[key_image]: ', key_image)
     
     return True
 
